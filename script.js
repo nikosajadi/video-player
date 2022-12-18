6e6e8d247fba4ab8411b08f4c6f774ab57ad4a47
nikosajadi <na.sajadii.29@gmail.com>
2022-12-18 21:54:03 +0100

    play/puse in video

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
-const video = document.querySelector('video');
+const player = document.querySelector('.player');
+const video = document.querySelector('.video');
 const progressRange = document.querySelector('.progress-range');
 const progressBar = document.querySelector('.progress-bar');
 // this constant for hour play button this time,I use querySelector because I want to change the classess
-const playBtn = document.qetElementById('play-btn');
+const playBtn = document.getElementById('play-btn');
 const volumeIcon = document.getElementById('volume-icon');
 const volumeRange = document.querySelector('.volume-range');
 const volumeBar = document.querySelector('.volume-bar');
@@ -11,3 +12,22 @@ const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullscreen');
 
+// play & pause 
+function togglePlay() {
+    if (video.paused) {
+      video.play();
+    // we going to change our playbtn when we click on it
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'pause');
+    } else {
+        video.pause();
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'pause');
+    }
+}
+
+
+
+// Event Listeners
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
