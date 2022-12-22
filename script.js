a5ec42b175628c68c70c463cf08da41bf5c7c985
nikosajadi <na.sajadii.29@gmail.com>
2022-12-22 23:51:36 +0100

    Build UI Controls HTML Show Controls on Hover

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+const player = document.querySelector('.player');
 const video = document.querySelector('.video');
 const progressRange = document.querySelector('.progress-range');
 const progressBar = document.querySelector('.progress-bar');
